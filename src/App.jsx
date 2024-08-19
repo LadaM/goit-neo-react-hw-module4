92c1241f3d6a1da76ca069131f2a748312150698
Lada <lada11lada@gmail.com>
2024-08-19 16:43:28 +0200

    4: styling header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import SearchBar from './components/SearchBar/SearchBar.jsx';
+import { useEffect, useState } from 'react';
 
 function App() {
-
+  const [searchQuery, setSearchQuery] = useState('');
+  useEffect(() => {
+    console.log(searchQuery);
+  }, [searchQuery]);
   return (
     <>
-      <SearchBar />
+      <SearchBar setSearchQuery={setSearchQuery} searchQuery={searchQuery} />
     </>
   )
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,20 +1,41 @@
-import css from './SearchBar.module.css'
+import css from './SearchBar.module.css';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => {
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+  const onSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    let query = event.currentTarget.elements.query.value.trim();
+    if (!query) {
+      return;
+    }
+    setSearchQuery(query);
+    event.currentTarget.reset();
+  };
+
   return (
-    <header>
-      <form className={css.container}>
+    <header className={css.header}>
+      <h1 className={css.title}>Search images</h1>
+      {searchQuery && <p className={css.query}>{searchQuery}</p>}
+      <form className={css.container} onSubmit={onSubmit}>
         <input
           type="text"
           className={css.input}
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos..."
+          name="query"
         />
-        <button type="submit">Search</button>
+        <button type="submit">
+          Search
+        </button>
       </form>
     </header>
   );
+};
+
+SearchBar.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  setSearchQuery: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
