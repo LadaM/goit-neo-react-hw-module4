9ed05d3cb8ef2037b635fb0ab92cd24cbf939236
Lada <lada11lada@gmail.com>
2024-08-25 12:31:56 +0200

    4: loader centered on top of the gallery with a backdrop

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,11 +1,17 @@
 import css from './LoadMoreBtn.module.css';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoadMoreBtn = ({ onClick, loading }) => {
   return (
     <button onClick={onClick} disabled={loading} className={css.loadMoreButton}>
       {loading ? 'Loading...' : 'Load More'}
     </button>);
+};
+
+LoadMoreBtn.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default LoadMoreBtn;
